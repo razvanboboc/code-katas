44daa9292fec3509bdf4d3de5f06ac71b3391d41
razvanboboc <rzvn11@gmail.com>
2020-10-09 01:17:35 +0300

    :sparkles: Finished last problem at end of day

--- day_15/reverse-or-rotate.js
+++ day_15/reverse-or-rotate.js
@@ -1,29 +1,40 @@
 function revrot(str, sz){
+    if(sz <= 0 || str === '' || sz > str.length){
+        return '';
+    }
     //divide string into chunks of size sz and form a new array with it
     //ignore last chunk if smaller than sz
     let chunks = [];
-    for(let i = 0; i < str.length; i += sz){
-        chunks.push(str.substring(i, i + sz));
+    for(let i = sz; i < str.length; i += sz){
+        chunks.push(str.substring(i - sz, i));
     }
     //iterate through array
-    chunks.forEach(function(chunk){
+    chunks = chunks.map(function(chunk){
         //verify if chunk is equal to the sum of all the cubes of its digits
         //divisible by 2
         chunk = chunk.split('');
         let sum = 0;
         for(i = 0; i < chunk.length; i++){
-            sum = sum + Math.pow(parseInt(chunk[i]), 3);
+            sum += Math.pow(parseInt(chunk[i]), 3);
+            // console.log(Math.pow(parseInt(chunk[i]), 3))
         }
-        if(sum)
-        console.log(sum); 
-    }) 
-    //if it is reverse it
-    //else rotate it to the left by one position
+        //if it is reverse it
+        if(sum % 2 === 0){
+            chunk = chunk.reverse();
+        } else {
+            //else rotate it to the left by one position
+            shiftedNumber = chunk.shift();
+            chunk.push(shiftedNumber);
+        } 
+        chunk = chunk.join('');
+        return chunk;
+    })
+    return chunks.join(''); 
 }
 
 
-console.log(revrot("123456987654", 6));
+// console.log(revrot("123456987654", 6));
 // console.log("1234", 0);
 // console.log("", 0);
 // console.log("1234", 5);
-// console.log("733049910872815764", 5);
+console.log(revrot("73304991087281576455176044327690580265896896028", 8));
