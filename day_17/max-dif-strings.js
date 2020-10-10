e4484cf394d45d128bf43088b6012e963e171706
razvanboboc <rzvn11@gmail.com>
2020-10-10 13:13:59 +0300

    :sparkles: Refactored code for one prob

--- day_17/max-dif-strings.js
+++ day_17/max-dif-strings.js
@@ -2,10 +2,7 @@ function mxdiflg(a1, a2) {
     let maxDiff = -1;
     for(let i = 0; i < a1.length; i++){
         for(let j = 0; j < a2.length; j++){
-            let diff = Math.abs(a1[i].length - a2[j].length);
-            if(diff > maxDiff){
-                maxDiff = diff;
-            }
+            maxDiff = Math.max(maxDiff, Math.abs(a1[i].length - a2[j].length)); 
         }
     }
     return maxDiff;
