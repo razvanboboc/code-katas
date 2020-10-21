e69529fb2ebe92da63e68d98718c3f4b5cd388ac
razvanboboc <rzvn11@gmail.com>
2020-10-21 22:04:50 +0300

    :sparkles: Solved 6kyu from day_26

--- day_26/loneliest-character.js
+++ day_26/loneliest-character.js
@@ -1,53 +1,44 @@
 function loneliest(str) {
-str = str.split('');
-//init obj to remember the chars and their loneliness
-let chars ={};
-//init loneliest variable
-let loneliest = -1;
-//go through every character of the array
-for(let i = 0; i < str.length; i++){
-    //init temp var to current i
-    let currentIndex = i;
-    // //init var to remember blanks
-    // let blanksCounter = 0;
-    //if the current char is not a blank
-    if(str[i] !== ' '){
-        //array takes current char as first element and its loneliness as 0
-        chars.str[i] = 0;
-    }
-    //while current character is not blank
-    while(str[i] !== ' '){
-        //verify if previous character was blank
-        if(str[currentIndex] == ' ') {
-            //blank var incremented
-            chars.[str[i]].++;
-            //temp var decremented
-            currentIndex--;
-        }
-    }
-    //temp var reinit with current index i
-    currentIndex = i;
-    //while current character is not blank
-    while(str[i] !== ' '){
-        //verify if next character is blank
-        if(str[currentIndex] == ' '){
-            //blank var incremented
-            blanksCounter++;
-            //temp var incremented
-            currentIndex--
-        }
-    }
-    //obj attribute takes the value from the blank var
-    chars.str[i]
-    //if blank var is greater than loneliest 
-        //loneliest becomes blank variable
-}
-//init new array to display loneliest characters
-//iterate through array of chars and their values
-    //verify if current attribute has an equal value to the loneliest char
-        //add element to loneliest array
-//return sorted array of characters
-        
+  str = str.trim().split('');
+  let chars = {};
+  let  loneliest = -1;
+  let loneliestChars = [];
 
+  for (let i = 0; i < str.length; i++) {
+    let leftIndex = i - 1;
+    let rightIndex = i + 1;
+    let blanksCounter = 0;
+
+    if (str[i] !== " " && !chars[str[i]]) {
+      chars[str[i]] = 0;
+    }
+    
+    while (str[leftIndex] === " " && str[i] !== ' ') {
+        blanksCounter++;
+        leftIndex--;
+    }
+    
+    while (str[rightIndex] === " " && str[i] !== ' ') {
+        blanksCounter++;
+        rightIndex++;
+    }
+
+    if(blanksCounter > chars[str[i]]){
+      chars[str[i]] = blanksCounter;
+    }
+
+    loneliest = Math.max(loneliest, blanksCounter);
+  }
+
+  for(let key in chars){
+    if(chars[key] === loneliest){
+      loneliestChars.push(key);
+    }
+  }
+  
+  return loneliestChars;
 }
 
+console.log(loneliest("     a  b  c de        "));
+// console.log(loneliest("     a     a"));
+console.log(loneliest("abc"));
