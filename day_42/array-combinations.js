4828c825e0697222071310f986d662ae31fffa9f
razvanboboc <rzvn11@gmail.com>
2020-11-07 14:59:33 +0200

    :sparkles: Worked on 6 kyu

---
+++ day_42/array-combinations.js
@@ -0,0 +1,8 @@
+function solve(arr) {
+    //counter for all combinations
+    //find a way to remove duplicates from all subarrays
+    
+};
+
+console.log(solve([[1,2],[4],[5,6]]));
+console.log(solve([[1,2],[4,4],[5,6,6]]));
