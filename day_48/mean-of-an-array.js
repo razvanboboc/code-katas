ea98d9bda5a0992dcb29dea22b79fcc22a9606a6
razvanboboc <rzvn11@gmail.com>
2020-11-13 23:34:49 +0200

    :sparkles: Solved 8 kyu

---
+++ day_48/mean-of-an-array.js
@@ -0,0 +1,12 @@
+function getAverage(marks){
+    let sum = 0;
+    for(let i = 0; i < marks.length; i++){
+      sum += marks[i];
+    }
+    return Math.floor(sum / marks.length);
+}
+
+
+console.log(getAverage([2,2,2,2]),2);
+console.log(getAverage([1,2,3,4,5,]),3);
+console.log(getAverage([1,1,1,1,1,1,1,2]),1);
