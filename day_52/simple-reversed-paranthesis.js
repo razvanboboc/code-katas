2b38bfc2f3f04cb1a48c03dcc7535b70fdf0c132
razvanboboc <rzvn11@gmail.com>
2020-11-19 11:43:14 +0200

    💗 Solved 8 kyu & 6 kyu from day 52

--- day_52/simple-reversed-paranthesis.js
+++ day_52/simple-reversed-paranthesis.js
@@ -1,31 +1,40 @@
-function solve(s){
-    s = s.split('');
-    let counter = 0;
-    let changed = 0;
-    if(s.length % 2 !== 0){
-        return -1;
+function solve(s) {
+  s = s.split("");
+  let counter = 0;
+  let changed = 0;
+  if (s.length % 2 !== 0) {
+    return -1;
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      counter++;
     }
-    for(let i = 0; i < s.length; i++){
-        if(s[i] === "("){
-            counter++;
-        }
-        if(s[i] === ")" && counter !== 0){
-            counter--;
-        } 
-        if(s[i] === ")" && counter === 0){
-            s[i] === "(";
-            changed++;
-            counter++;
-        }
-        
+    if (s[i] === ")" && counter !== 0) {
+      counter--;
+    } else if (s[i] === ")" && counter === 0) {
+      s[i] = '(';
+      changed++;
+      counter++;
     }
-    return counter;
+  }
+  let halfCounter = counter / 2;
+  let index = s.length - 1;
+  while (halfCounter !== 0) {
+      s[index] = ")";
+      changed++;
+      halfCounter--;
+      index--;
+  }
+  return changed;
 }
 
-
-// console.log(solve("())()))))()()("));
-// ( ) ) ( ) ) ) ) ) ( ) ( ) ( ( ( 
-// // console.log(solve(")()("));
-// console.log(solve("((()"));
+console.log(solve("())()))))()()("));
+// console.log("( ) ( ( ) ) ( ) ( ( ) ( ) )")
+// ( ) ) ( ) ) ) ) ) ( ) ( ) ( ( (
+// ( )
+console.log(solve(")()("));
+console.log(solve("((()"));
 console.log(solve("())((("));
-// ( ) ) ( ( ( 
+// ( ) ) ( ( (
+
+// (((()()(
