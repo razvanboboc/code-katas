8d1f1a810b1cf542564cfcf4c10e2c3a5294119c
razvanboboc <rzvn11@gmail.com>
2020-11-27 15:30:06 +0200

    :sparkles: Worked on 6 kyu

--- day_59/bonuses.js
+++ day_59/bonuses.js
@@ -1,9 +1,19 @@
 bonus = function(arr, s) {
-    let sum = 0;
-    arr.forEach(e => sum += e);
-    let bonusPerDay = Math.floor(s / sum);
-    // return arr.map(e => Math.floor(e * bonusPerDay));
-    return bonusPerDay;
+    let k = 0;
+    arr.forEach(e => k *= e);
+    k /= arr.length;
+    return arr.map(e => Math.floor(e * 100 / sum) );
 }
 
+
+//am un bonus total x.
+//trebuie sa-l impart pe mai multe procente din x, care insumate, 
+//sa dea tot x. 
+
+// 12 -- 40.54%
+// 15 -- 32.43%
+// 18 -- 27.02%
+
+//i need to devise a formula, to split the grand total of bonus
+//into different bonuses.
 console.log(bonus([18, 15, 12], 851));
