0bcd64f00c2fa33d5eab949d6f60482d31b82a3d
razvanboboc <rzvn11@gmail.com>
2020-11-28 16:59:49 +0200

    :sparkles: Worked on 6 kyu

--- day_59/bonuses.js
+++ day_59/bonuses.js
@@ -1,19 +1,47 @@
 bonus = function(arr, s) {
-    let k = 0;
-    arr.forEach(e => k *= e);
-    k /= arr.length;
-    return arr.map(e => Math.floor(e * 100 / sum) );
+    let commonDenominator = 1;
+    arr.forEach(e => commonDenominator *= e);
+    let numerator = commonDenominator * s;
+    let arrOfKs = arr.map(e => commonDenominator / e);
+    let calcSumOfKs = (accumulator, currentElement) => accumulator + currentElement;
+    let sumOfKs = arrOfKs.reduce(calcSumOfKs);
+    let k = numerator / sumOfKs;
+    arr = arr.map(e => Math.floor(k / e));
+    return arr;
+}
+console.log(bonus([
+    19, 29, 15, 7, 10,
+    28, 10, 11, 9, 26
+  ], 383631715));
+
+
+/*
+//debugging code
+
+bonus = function(arr, s) {
+    console.log(arr);
+    console.log(s);
+    let commonDenominators = 1;
+    for(let i = 0; i < arr.length; i++){
+      if(commonDenominator * arr[i] < Number.MAX_VALUE){
+        commonDenominator *= arr[i];
+      } else {
+        
+      }
+    }
+    arr.forEach(e => commonDenominator *= e);
+    console.log(`Common Denom is : ${commonDenominator}`)
+    let numerator = commonDenominator * s;
+    let arrOfKs = arr.map(e => commonDenominator / e);
+    console.log(`Arr of Ks is : ${arrOfKs}`)
+    let calcSumOfKs = (accumulator, currentElement) => accumulator + currentElement;
+    let sumOfKs = arrOfKs.reduce(calcSumOfKs);
+    let k = numerator / sumOfKs;
+    console.log(`SumOfKs is ${sumOfKs}`)
+    console.log(`Numerator is ${numerator}`)
+    console.log(`K is ${k}`)
+    arr = arr.map(e => Math.floor(k / e));
+    return arr;
 }
 
-
-//am un bonus total x.
-//trebuie sa-l impart pe mai multe procente din x, care insumate, 
-//sa dea tot x. 
-
-// 12 -- 40.54%
-// 15 -- 32.43%
-// 18 -- 27.02%
-
-//i need to devise a formula, to split the grand total of bonus
-//into different bonuses.
-console.log(bonus([18, 15, 12], 851));
+*/
