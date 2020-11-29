f5215180f933ad408b158bea18ac14328908f311
razvanboboc <rzvn11@gmail.com>
2020-11-29 08:16:13 +0200

    :sparkles: Worked on 6 kyu

--- day_59/bonuses.js
+++ day_59/bonuses.js
@@ -1,19 +1,42 @@
 bonus = function(arr, s) {
+    arr = simplestFormArray(arr);
     let commonDenominator = 1;
     arr.forEach(e => commonDenominator *= e);
     let numerator = commonDenominator * s;
     let arrOfKs = arr.map(e => commonDenominator / e);
-    let calcSumOfKs = (accumulator, currentElement) => accumulator + currentElement;
+    let calcSumOfKs = (totalSum, currentElement) => totalSum + currentElement;
     let sumOfKs = arrOfKs.reduce(calcSumOfKs);
     let k = numerator / sumOfKs;
     arr = arr.map(e => Math.floor(k / e));
     return arr;
 }
-console.log(bonus([
-    19, 29, 15, 7, 10,
-    28, 10, 11, 9, 26
-  ], 383631715));
 
+function simplestFormArray(arr){
+  arr = arr.sort( (a,b) => {
+    if(a < b) return 1;
+    if(a > b) return -1;
+    return 0;
+  });
+  for(let i = arr[0]; i > 1 ; i--){
+    let areDivisible = true;
+    arr.forEach(e => {
+      if(e % i !== 0){
+        areDivisible = false;
+      }
+    });
+    if(areDivisible){
+      arr = arr.map(e => e / i);
+    }
+  }
+  return arr;
+}
+
+// console.log(bonus([
+//     19, 29, 15, 7, 10,
+//     28, 10, 11, 9, 26
+//   ], 383631715));
+
+console.log(simplestFormArray([18, 15, 12]));
 
 /*
 //debugging code
