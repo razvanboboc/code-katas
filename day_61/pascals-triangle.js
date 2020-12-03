e3e0e32897e2379a3fc602e4efc08ddf65dfd5d9
razvanboboc <rzvn11@gmail.com>
2020-12-03 23:00:58 +0200

    :sparkles: Completed 6 kyu

--- day_61/pascals-triangle.js
+++ day_61/pascals-triangle.js
@@ -9,26 +9,33 @@ function pascalsTriangle(n) {
         }
         row++;
     } 
-    return arr[arr.length - 2];   
+    return arr;   
 }
 
 function generateElement(row, column){
-    let numerator = fac(row);
-    console.log(numerator)
-    let denominator = fac(column) * fac(row - column);
-    console.log(denominator)
-    return numerator / denominator;
+    let a = calculateNfacOverKfac(row, column);
+    let b = 1 / fac(row - column);
+    return Math.round(a * b);
+}
+
+function calculateNfacOverKfac(n, k){
+    k += 1;
+    let sol = 1;
+    while(k <= n){
+        sol *= k;
+        k++;
+    }
+    return sol;
 }
 
 function fac(n){
     return n < 2 ? 1 : n * fac(n - 1);
 }
 
-
+// console.log(generateElement(2, 0))
 // console.log(pascalsTriangle(1))
 // console.log(pascalsTriangle(2))
-// console.log(pascalsTriangle(4))
+console.log(pascalsTriangle(20))
 // console.log(pascalsTriangle(6))
-console.log(generateElement(24, 23))
 // console.log(pascalsTriangle(25))
 // console.log(generateElement(4, 2))
