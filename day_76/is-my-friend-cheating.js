5e919b3aba804f3108b5bbb3fee472e1892da9ed
razvanboboc <rzvn11@gmail.com>
2020-12-29 01:20:33 +0200

    :sparkles: Optimized solution

--- day_76/is-my-friend-cheating.js
+++ day_76/is-my-friend-cheating.js
@@ -1,35 +1,33 @@
 function removeNb(n) {
-    //fac Math.sqrt(suma)
-    //din numarul dat fac Math.floor(rezultat)
-    //targetSum = rezultat + rezultat.
-    //caut pe langa valoarea sqrt cele doua numere care adaugate
-    //sa dea targetSum
-        //apoi verific daca inmultite dau suma - ele.
+    //incepem de la n/2 pana la n
     let sum = n * (n + 1) / 2;
     //create a result array with pairs of numbers
     let pairs = [];
     //go through every number from greatest
     //i^2 = sum - 2 * i
-    for(let i = n; i > 0 && i > Math.sqrt(sum - 2 * i); i--){
+    for(let i = Math.floor(n / 2); i < n; i++){
         //pair it with another smaller number number
-        for(let j = i - 1; j > 0; j--){
+        for(let j = i + 1; j <= n; j++){
+            process.stdout.write(`i is = ${i} `);
+            process.stdout.write(`j is = ${j} \n`);
             //calculate their product
             let product = i * j;
             //substract from sum a and b.
             let substractedSum = sum - i - j;
-            if(product < substractedSum){
+            process.stdout.write(`p is = ${product} `);
+            process.stdout.write(`s is = ${substractedSum} \n`);
+            if(product > substractedSum){
                 break;
             }
             //verify if their product is equal to substracted sum
             if(product === substractedSum){
                 //if it is then add it to the result array
-                pairs.push([j, i]);
                 pairs.push([i, j]);
             }
         }
     }
-    return pairs.sort((a, b) => a[0] - b[0]);
+    return pairs;
 }
 
 console.log(removeNb(26))
-console.log(removeNb(100))
+// console.log(removeNb(1000003))
