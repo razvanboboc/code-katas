0e9a0033aed4c591b4a079a878f7a4807374205d
razvanboboc <rzvn11@gmail.com>
2021-01-02 23:55:38 +0200

    :sparkles: Worked on 5 kyu

---
+++ day_80/john-and-ann.js
@@ -0,0 +1,20 @@
+function john(n) {
+    //generate both john and ann arrays as I go along
+    //both generated arrays are dependent on each other 
+    //for their generation -- they use the last element from
+    //the other array
+}
+function ann(n) {
+
+}
+
+function sumJohn(n) {
+
+}
+
+function sumAnn(n) {
+
+}
+
+console.log(john((11, [0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 6])));
+console.log(ann((6, [1, 1, 2, 2, 3, 3])));
