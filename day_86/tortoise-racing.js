43289de098b8d3b9a5c0aea9a6982dc8c6178609
razvanboboc <rzvn11@gmail.com>
2021-01-10 16:07:43 +0200

    :sparkles: Solved 6 kyu

--- day_86/tortoise-racing.js
+++ day_86/tortoise-racing.js
@@ -1,27 +1,36 @@
 function race(v1, v2, g) {  
     let speedOfClosingGap = v2 - v1;
     let timeToCloseGap = g / speedOfClosingGap * 3600;
-    console.log(timeToCloseGap)
+
     let result = [];
     let hours = 0,
         minutes = 0,
         seconds = 0;
     if(timeToCloseGap >= 3600){
-        timeToCloseGap /= 3600;
-        hours = timeToCloseGap;
+        hours = Math.round(timeToCloseGap / 3600);
+        timeToCloseGap = timeToCloseGap - hours * 3600;
         result.push(hours);
+    } else {
+        result.push(0);
     }
     if(timeToCloseGap < 3600 && timeToCloseGap > 59){
-        timeToCloseGap /= 60;
-        minutes = Math.round(timeToCloseGap); 
+        minutes = Math.floor(timeToCloseGap / 60); 
+        timeToCloseGap -= minutes * 60;
         result.push(minutes)
+    } else {
+        result.push(0)
     }
     if(timeToCloseGap < 60){
-        seconds = timeToCloseGap - minutes; 
+        seconds = Math.round(timeToCloseGap); 
         result.push(seconds)
+    } else {
+        result.push(0)
     }
     
     return result;
 }
 
-console.log(race(720, 850, 70));
+// console.log(race(720, 850, 70));
+// console.log(race(80, 91, 37))
+// console.log(race(80, 100, 40));
+console.log(race(820, 81, 550))
