49ebd5a4adcf6c9257ada1b82a53595f7bcf3e80
razvanboboc <rzvn11@gmail.com>
2021-01-11 13:50:52 +0200

    :sparkles: Worked on 5 kyu

--- day_87/buddy-pairs.js
+++ day_87/buddy-pairs.js
@@ -4,29 +4,31 @@ function buddy(start,limit) {
     //go and try through trial and error what numbers are buddies
     
     //condition to satisfy 
-    //sum divisors of n = m + 1
-    //sum divisors of m = n + 1
-
-    //init numbers array
-    let numbers = [];
-    //populate array with numbers and sum of proper divisors.
-    for(let i = start; i <= limit; i++){
-        numbers.push([i, sumProperDivisors(i)]);
+    //sum divisors of n = m + 1 sa mearga pana la sum - 1
+    //sum divisors of m = n + 1 sa mearga pana la sum - 1
+    // let numbers = [];
+    // for(let i = start; i <= limit; i++){
+    //     numbers.push([i, sumProperDivisors(i)])
+    // }
+    // return numbers;
+    for(let i = start; i <= limit ; i++)
+    { 
+        let currentDivisorSum = sumProperDivisors(i);
+        let buddy = currentDivisorSum - 1;
+        if(sumProperDivisors(buddy) - 1 === i){
+            return [i, buddy];
+        }
     }
-    console.log(numbers);
-    //go through populated array.
-        //find buddy in start limit interval
-        //if not found and sum of its divisors is larger than limit + 1 
-            //try to look in outer 
-        //if not found and sumDiv(n) < limit + 1
-            //next element
-    
-    return [,];
     return "Nothing";
 }
 
 function sumProperDivisors(n){
+    if(n === 0){
+        return 0;
+    }
+    // console.log(n)
     let divisors = [];
+    // console.log(divisors)
     for(let i = 1; i <= n / 2; i++){
         if(n % i === 0){
             divisors.push(i);
@@ -36,6 +38,12 @@ function sumProperDivisors(n){
     return sum;
 }
 
-// console.log(sumProperDivisors(48))
+// console.log(sumProperDivisors(23))
 // console.log(sumProperDivisors(75))
-console.log(buddy(48,75))
+// console.log(buddy(48,75))
+// console.log(buddy(48,75))
+// console.log(buddy(23, 4669))
+
+console.log(buddy(4952, 6539), [5775, 6128] );
+console.log(buddy(381, 4318), [1050, 1925] );
+console.log(buddy(2382, 3679), "Nothing" );
