b9e134727a4ee6ce128b049bf78b844d80397c0f
razvanboboc <rzvn11@gmail.com>
2021-01-12 08:56:58 +0200

    :sparkles: Solved 5 kyu

--- day_87/buddy-pairs.js
+++ day_87/buddy-pairs.js
@@ -1,20 +1,9 @@
 function buddy(start,limit) {
-    //condition to satisfy 
-    //sum divisors of n = m + 1 sa mearga pana la sum - 1
-    //sum divisors of m = n + 1 sa mearga pana la sum - 1
-    //sumDiv(n) = m - 1
-    //sumDiv(m) = n - 1
-
-    // let numbers = [];
-    // for(let i = start; i <= limit; i++){
-    //     numbers.push([i, sumProperDivisors(i)])
-    // }
-    // return numbers;
     for(let i = start; i <= limit ; i++)
     { 
         let currentDivisorSum = sumProperDivisors(i);
         let buddy = currentDivisorSum - 1;
-        if(sumProperDivisors(buddy) - 1 === i){
+        if(sumProperDivisors(buddy) - 1 === i && buddy > i){
             return [i, buddy];
         }
     }
@@ -34,13 +23,3 @@ function sumProperDivisors(n){
     }
     return divisors.reduce((acc,cur) => acc + cur);
 }
-
-// console.log(sumProperDivisors(48))
-// console.log(sumProperDivisors(75))
-// console.log(buddy(48,75))
-// console.log(buddy(48,75))
-// console.log(buddy(23, 4669))
-
-console.log(buddy(4952, 6539), [5775, 6128] );
-console.log(buddy(381, 4318), [1050, 1925] );
-console.log(buddy(2382, 3679), "Nothing" );
