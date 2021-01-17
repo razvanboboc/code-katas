791da459a92ab665489e3e9c7e6d11bf0711621b
razvanboboc <rzvn11@gmail.com>
2021-01-17 13:13:24 +0200

    :sparkles: Solved 7 kyu

--- day_89/apparently-modifying-strings.js
+++ day_89/apparently-modifying-strings.js
@@ -1,19 +1,17 @@
 function apparently(string) {
-    let result =  string.split(' ').map((x) => {
-      if(x === 'but' || x === 'and'){
-        return x + ' apparently';
+  let result = string.split(" ");
+  console.log(result);
+  return result
+    .map((x, i) => {
+      if (
+        (result[i] === "but" || result[i] === "and") &&
+        result[i + 1] != "apparently"
+      ) {
+        return x + " apparently";
       }
       return x;
-    });
-    console.log(result)
-    for(let i = 0; i < result.length; i++){
-        console.log(result[i])
-        if(result[i] === 'apparently' && result[i+1] === 'apparently'){
-            result.splice(i, 1);
-            i--;
-        }
-    }
-    return result.join(' ');
-  }
+    })
+    .join(" ");
+}
 
-console.log(apparently('but apparently'));
+console.log(apparently("and EmGv and but apparently LpABZ lCq but"));
