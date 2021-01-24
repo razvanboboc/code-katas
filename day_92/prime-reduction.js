e607732af5e573ec207721dedaba8ea41cde2c5e
razvanboboc <rzvn11@gmail.com>
2021-01-24 16:34:08 +0200

    :sparkles: Worked on 6 kyu

--- day_92/prime-reduction.js
+++ day_92/prime-reduction.js
@@ -1,14 +1,50 @@
 function solve(a, b) {
-    return 0;
+    let counter = 0;
+    for (let i = a; i < b; i++) {
+        if (isPrime(i) && primeReducer(i)) {
+            counter++;
+        }
+    }
+    return counter;
 }
 
-function primeReducer(n){
-    n = n.toString().split('').map(x => Number.parseInt(x)**2).reduce((acc,cur) => acc + cur);   
-    //go through digits
-        //calculate square of each one of them
-        //reduce them ito a sum
-    //if the sum is not 1 then we could do a primeReducer(sum)
+function primeReducer(n) {
+    n = n
+        .toString()
+        .split("")
+        .map((x) => Number.parseInt(x) ** 2)
+        .reduce((acc, cur) => acc + cur);
+    if (n < 10 && n != 1 && n != 7) {
+        return false;
+    }
     return n === 1 ? true : primeReducer(n);
 }
 
-console.log(primeReducer(7))
+function isPrime(n) {
+    if (n === 0 || n === 1) {
+        return false;
+    }
+    for (let i = 2; i < Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// console.log(primeReducer(1));
+// console.log(primeReducer(2));
+// console.log(primeReducer(3));
+// console.log(primeReducer(4));
+// console.log(primeReducer(5));
+// console.log(primeReducer(6));
+// console.log(primeReducer(7), 7);
+// console.log(primeReducer(8));
+// console.log(primeReducer(9));
+// console.log(primeReducer(23));
+// console.log(primeReducer(19));
+// console.log(primeReducer(49999));
+
+// console.log(solve(100,1000),28);
+// console.log(solve(1,25), 4);
+console.log(solve(100, 3000), 65);
