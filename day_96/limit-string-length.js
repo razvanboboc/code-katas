53daa9e5bd869986114d9948115b49bd406dcd7c
razvanboboc <rzvn11@gmail.com>
2021-01-28 23:27:30 +0200

    :sparkles: Solved 7  kyu

--- day_96/limit-string-length.js
+++ day_96/limit-string-length.js
@@ -1,1 +1,5 @@
-function solution(string, limit) {}
+function solution(string, limit) {
+  return limit >= string.length ? string : string.substring(0, limit) + "...";
+}
+
+console.log(solution("Test", 4));
